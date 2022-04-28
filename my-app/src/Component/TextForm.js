56a4372a49df4ee6efaee7afd2eb8a7096247f5a
Amrit Poudel <amritpoudel433@gmail.com>
2022-04-28 22:33:10 +0545

    Fixing state

--- my-app/src/Component/TextForm.js
+++ my-app/src/Component/TextForm.js
@@ -1,14 +1,15 @@
-import React ,{useState} from 'react'
+import React , {useState} from 'react'
 
 export default function TextForm(props) {
 
 
 
-    // const [ text , setText] = useState ( 'Enter Your Text Here ');
+    
 
      const [text, setText] = useState('Enter Your Text Here ');
 
       const handleUpclick = ()=> {
+           
           console.log("Upper Case is clicked" , text );
           
   
@@ -31,7 +32,7 @@ export default function TextForm(props) {
 <div className="mb-3">
      <h1> {props.heading }</h1>
      <textarea className="form-control" value={text} onChange={handleOnchange} id="myBox" rows="6"></textarea>
-     
+
 </div>
 
    <button className="btn btn-primary"  onClick={handleUpclick}> Convert To Upperclass</button>
